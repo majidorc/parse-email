a6194cbb5d058eeccb0110c9a45f04ce826b02b9
Majid oRc <o0dr.orc0o@gmail.com>
2025-07-05 21:21:43 +0700

    Include book_date in bookings and accounting API responses

--- api/accounting.js
+++ api/accounting.js
@@ -98,7 +98,7 @@ module.exports = async (req, res) => {
 
     // Use string interpolation for ORDER BY direction
     let dataQuery = `
-      SELECT booking_number, tour_date, customer_name, sku, program, hotel, paid
+      SELECT booking_number, book_date, tour_date, customer_name, sku, program, hotel, paid
       FROM bookings
       ${whereClause}
       ORDER BY ${sort} ${dirStr}
